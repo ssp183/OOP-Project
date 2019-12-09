16d9c8b3d369fd53e576ca98c0a52c0d26def0fe
aakashp2302 <58701545+aakashp2302@users.noreply.github.com>
2019-12-09 13:28:52 -0500

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -4,7 +4,28 @@ public class Transactions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner scan = new Scanner (System.in);
+float balance= 0;
+float deposit;
+float withdraw;
+int additionTransaction;
+int nextStep;
 
+	System.out.println("Please choose a service:");
+	System.out.println("1. Deposit:");
+	System.out.println("2. Withdraw:");
+	System.out.println("3. Check Balance:");
+	 
+	nextStep= scan.nextInt();
+	
+
+switch(nextStep) {
+case 1:
+	System.out.println("Please enter the amount you would like to deposit:");
+	deposit= scan.nextFloat();
+	balance+= deposit;
+	System.out.println("You deposited:" +deposit);
+	System.out.println("New balance is:" +balance);
+}
 	}
-
 }
