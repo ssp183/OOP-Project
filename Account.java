989980a0e56677e1763d0e20ba26284bf0a33de3
hbushe <57971202+hbushe@users.noreply.github.com>
2019-12-09 13:40:11 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 public class Account {
 	
 	
-	public boolean Verification () {
+	public Verification () {
 		boolean verified;
 		  //USER ENTERS ACCOUNTNUMBER
 		Scanner accountnum = new Scanner(System.in);
 		System.out.println("Enter your sixteen digit account number: "); 
 		int accountNumber= accountnum.nextInt();
+		accountnum.close();
 		int truePin= ATMSystem(accountNumber);
 		
       		  //USER ENTERS PIN
 		Scanner input = new Scanner(System.in);
       		System.out.println("Enter your four digit pin number: "); 
        		int pin = input.nextInt();
+		input.close();
 		
 		  //SYSTEM VERIFIES WITH TRUEPIN FROM ATM SYSTEM
 		if (pin == truePin) {
@@ -25,7 +27,11 @@ public class Account {
 		}
 		
 		  //SYSTEM ALLOWS USER INTO SYSTEM
-		
+		if (verified) {
+			return 
+		} else {
+			return 
+		}
 		
 			
 	}
