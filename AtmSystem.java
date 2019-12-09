fea8eb6ce5c25308dc4b76e84de5775d58050bca
ssp183 <58701066+ssp183@users.noreply.github.com>
2019-12-09 13:11:09 -0500

    Update AtmSystem.java

--- AtmSystem.java
+++ AtmSystem.java
@@ -2,8 +2,10 @@
 import java.util.Scanner;
 public class AtmSystem {
 	public static void main(String[] args) {
-		// main class
-
+		// main class (calls from Account)
+	System.out.println("Welcome to the ATM System, enter ATM pin");
+	Scanner scan = new Scanner(System.in);
+		
 	}
 
 }
