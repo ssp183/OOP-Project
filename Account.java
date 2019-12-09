6ce50c69c0f7909b915dfda86a44782c04d8401c
hbushe <57971202+hbushe@users.noreply.github.com>
2019-12-09 13:16:17 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,6 +1,22 @@
+import java.util.Date;
+import java.util.Scanner;
 
 public class Account {
-
+	
+	
+	public boolean Verification {
+		
+	Scanner input = new Scanner(System.in);
+	int accountNumber ;
+        //USER ENTERS PIN
+        System.out.println("Enter your four digit pin number: "); 
+        int pin = input.nextInt();
+	ATMSystem(accountNumber, pin);
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
